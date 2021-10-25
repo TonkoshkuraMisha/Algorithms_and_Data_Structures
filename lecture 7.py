201f7b86bfffcbeddc6b90f30bd0d3c04538f598
TonkoshkuraMisha <tonkoshkura.misha@gmail.com>
2021-10-25 15:56:01 +0300

    lecture 7 files was changed

--- lecture 7.py
+++ lecture 7.py
@@ -103,15 +103,15 @@ print(pow2(3, pow2(3, 3)))
 
 # Hanoi tower
 
-def moveTower(height,fromPole, toPole, withPole):
+def moveTower(height, fromPole, toPole, withPole):
     if height >= 1:
-        moveTower(height-1,fromPole,withPole,toPole)
-        moveDisk(fromPole,toPole)
-        moveTower(height-1,withPole,toPole,fromPole)
-
-def moveDisk(fp,tp):
-    print("moving disk from",fp,"to",tp)
+        moveTower(height - 1, fromPole, withPole, toPole)
+        moveDisk(fromPole, toPole)
+        moveTower(height - 1, withPole, toPole, fromPole)
 
 
+def moveDisk(fp, tp):
+    print("moving disk from", fp, "to", tp)
 
-moveTower(3,"A","B","C")
+
+moveTower(3, "A", "B", "C")

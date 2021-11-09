bda7e6de824ac4ee583668e88947acfb91a53318
TonkoshkuraMisha <tonkoshkura.misha@gmail.com>
2021-11-09 09:50:00 +0200

    l26 was added

--- lecture 25.py
+++ lecture 25.py
@@ -2,38 +2,21 @@
 Очередь FIFO. Для добавления и удаления принято использовать комманды add и remove.
 Стек LIFO. Для добавления и удаления элементов принято использовать push и pop.
 """
-# эффективная реализация очереди
-"""from collections import deque
-queue = deque(range(100_000))
-queue.append(1)
-
-%%time
-x = queue.popleft()"""
 """
 N, M = map(int, input().split())
-G = {i: set() for i in range(N)}
+graph = {i: set() for i in range(N)}
 for i in range(M):
     v1, v2 = map(int, input().split())
-    G[v1].add(v2)
-    G[v2].add(v1)
-
-from collections import deque
-distances = [None]*N
-start_vertex = 0
-distances[start_vertex] = 0
-queue = deque([start_vertex])
-
-while queue:
-    cur_v = queue.popleft()
-    for neigh_v in G[cur_v]:
-        if distances[neigh_v] is None:
-            distances[neigh_v] = distances[cur_v] + 1
-            queue.append(neigh_v)
+    graph[v1].add(v2)
+    graph[v2].add(v1)
 """
+
+
 
 # Восстановление траектории движения коня.
 letters = 'abcdefgh'
 numbers = '12345678'
+
 graph = dict()
 for i in letters:
     for j in numbers:
@@ -63,4 +46,34 @@ for i in range(8):
         if 0 <= i - 1 < 8 and 0 <= j + 2 < 8:
             v2 = letters[i - 1] + numbers[j + 2]
             add_edge(v1, v2)
+
+
+from collections import deque
+distances = {v: None for v in graph}
+parents = {v: None for v in graph}
+
+start_vertex = 'a1'
+end_vertex = 'h8'
+
+distances[start_vertex] = 0
+queue = deque([start_vertex])
+
+
+while queue:
+    cur_v = queue.popleft()
+    for neigh_v in graph[cur_v]:
+        if distances[neigh_v] is None:
+            distances[neigh_v] = distances[cur_v] + 1
+            parents[neigh_v] = cur_v
+            queue.append(neigh_v)
+
+
+
+path = [end_vertex]
+parent = parents[end_vertex]
+while not parent is None:
+    path.append(parent)
+    parent = parents[parent]
+
 print(graph)
+print(path[::-1])
